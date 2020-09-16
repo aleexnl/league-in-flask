9179f6b77e8f2cde24b1eca2edee25388d8aabc0
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-09-16 13:20:10 +0200

    matrix and team list creation

--- app.py
+++ app.py
@@ -1,24 +1,52 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
-teams = []
+
+
+def load_teams():
+    team_list = []
+    file = open("teams.cfg", "r")
+    for team in file:
+        team_list.append(team.rstrip("\n"))
+    return team_list
+
+
+def create_league():
+    """ Creates the league matrix. """
+    league_dic = {}
+    ranking_dic = {}
+    for local_team in teams:
+        league_dic[local_team] = {}
+        ranking_dic[local_team] = 0
+        for visitant_team in teams:
+            league_dic[local_team][visitant_team] = None
+    return league_dic, ranking_dic
+
+
+teams = load_teams()
+league, ranking = create_league()
 
 
 @app.route('/')
-def input_teams():
+def set_goals():
+    return "Hello World!"
+
+
+@app.route('/league')
+def view_league():
     return """
-    <h1>ADD A TEAM</h1>
-    <form method="post">
-        <label for="team_name">Team name:</label>
-        <input type="text" name="team_name" id="team_name" />
-        <br>
-        <input type="submit">
-    </form>
-    """
+    <h1>league Chart</h1>
+    <p>{}</p>
+    <a href="/">Return</a>
+    """.format(league)
 
 
-@app.route('/', methods=['POST'])
-def method_name():
-    return 'Hello World!'
+@app.route('/team-list')
+def team_list():
+    return """
+    <h1>Team List</h1>
+    <p>{}</p>
+    <a href="/">Return</a>
+    """.format(", ".join(teams))
 
 
 if __name__ == '__main__':
