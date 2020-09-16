a68588cf0bbc35b05cce5c1957f56452097396a0
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-09-16 12:42:44 +0200

    FrontEnd: add teams

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
+teams = []
 
 
 @app.route('/')
-def hello():
+def input_teams():
+    return """
+    <h1>ADD A TEAM</h1>
+    <form method="post">
+        <label for="team_name">Team name:</label>
+        <input type="text" name="team_name" id="team_name" />
+        <br>
+        <input type="submit">
+    </form>
+    """
+
+
+@app.route('/', methods=['POST'])
+def method_name():
     return 'Hello World!'
 
 
