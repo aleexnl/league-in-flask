2956c8a5cce9b8bdf7b772f13b6c75b92b2330ab
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-09-18 16:16:33 +0200

    added team selection

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+top_menu_html = """
+<nav>
+<a href="/">Home</a>
+<a href="/goals">Goals</a>
+<a href="/league">League</a>
+<a href="/teams">Teams</a>
+</nav>
+"""
+
 
 def load_teams():
     team_list = []
@@ -22,27 +31,71 @@ def create_league():
     return league_dic, ranking_dic
 
 
+def create_html_team_options():
+    html = ""
+    for team in teams:
+        tag = "<option value=\"{}\">".format(team)
+        tag += team
+        tag += "</option>"
+        html += tag
+    return html
+
+
+def create_html_team_select(team_type):
+    team_list = create_html_team_options()
+    html = "<select name={}>".format(team_type)
+    html += team_list
+    html += "</select>"
+    return html
+
+
+def create_html_set_goals(local, visitant):
+    html = "<h1>{} vs {}</h1>".format(local, visitant)
+    return html
+
+
 teams = load_teams()
 league, ranking = create_league()
 
 
 @app.route('/')
-def set_goals():
-    return "Hello World!"
+def index():
+    return top_menu_html
+
+
+@app.route('/goals')
+def select_teams():
+    html = "<h1>Local Team</h1>"
+    html += """<form method="post">"""
+    html += create_html_team_select("local")
+    html += "<h1>Visitant Team</h1>"
+    html += create_html_team_select("visitant")
+    html += """</br>"""
+    html += """<input type="submit">"""
+    html += """</form>"""
+    return top_menu_html + html
+
+
+@app.route('/goals', methods=["POST"])
+def select_teams_post():
+    local_team = request.form["local"]
+    visitant_team = request.form["visitant"]
+    html = create_html_set_goals(local_team, visitant_team)
+    return html
 
 
 @app.route('/league')
 def view_league():
-    return """
+    return top_menu_html + """
     <h1>league Chart</h1>
     <p>{}</p>
     <a href="/">Return</a>
     """.format(league)
 
 
-@app.route('/team-list')
+@app.route('/teams')
 def team_list():
-    return """
+    return top_menu_html + """
     <h1>Team List</h1>
     <p>{}</p>
     <a href="/">Return</a>
