deef2b5b5bf93f85bb942a13ce1df55faa09d2f1
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-09-21 12:53:31 +0200

    Added ranking

--- app.py
+++ app.py
@@ -14,16 +14,22 @@ def load_teams():
 def create_league():
     """ Creates the league matrix."""
     league_dic = {}
-    ranking_dic = {}
     for local_team in teams:
         league_dic[local_team] = {}
-        ranking_dic[local_team] = 0
         for visitant_team in teams:
             if visitant_team == local_team:
                 league_dic[local_team][visitant_team] = "X"
             else:
-                league_dic[local_team][visitant_team] = ""
-    return league_dic, ranking_dic
+                league_dic[local_team][visitant_team] = None
+    return league_dic
+
+
+def create_ranking():
+    """Create ranking dic."""
+    ranking_dic = {}
+    for local_team in teams:
+        ranking_dic[local_team] = 0
+    return ranking_dic
 
 
 def update_league(local_team, visitant_team, local_goals, visitant_goals):
@@ -32,8 +38,49 @@ def update_league(local_team, visitant_team, local_goals, visitant_goals):
     league[visitant_team][local_team] = visitant_goals
 
 
+def update_ranking():
+    """TODO: Work on a better function."""
+    global ranking
+    ranking = create_ranking()
+    done_teams = []
+    for local_team in league:
+        for visitant_team in league[local_team]:
+            if league[local_team][visitant_team] is not None:
+                if local_team and visitant_team not in done_teams and local_team != visitant_team:
+                    points = check_ranking_points(
+                        league[local_team][visitant_team], league[visitant_team][local_team])
+                    set_ranking_points(points, local_team, visitant_team)
+        done_teams.append(local_team)
+
+
+def check_ranking_points(local, visitant):
+    """Check how many points did the local team win."""
+    if local != None:
+        if local > visitant:
+            return 3
+        elif local == visitant:
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+
+
+def set_ranking_points(points, local_team, visitant_team):
+    """Sum the points depending on the condition."""
+    if points == 3:
+        ranking[local_team] += points
+    elif points == 1:
+        ranking[local_team] += points
+        ranking[visitant_team] += points
+    else:
+        ranking[local_team] += points
+        ranking[visitant_team] += 3
+
+
 teams = load_teams()
-league, ranking = create_league()
+league = create_league()
+ranking = create_ranking()
 
 
 @app.route('/')
@@ -58,7 +105,8 @@ def goals_input(error=None):
 
 @app.route('/ranking')
 def ranking_chart(error=None):
-    return render_template("ranking.html", ranking=ranking)
+    update_ranking()
+    return render_template("ranking.html", ranking=sorted(ranking.items(), key=lambda x: x[1], reverse=True))
 
 
 @app.route('/goals', methods=["POST"])
